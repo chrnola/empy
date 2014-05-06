7624d19102431b228b36c958c722135ed08a3098
Chris Pinola <chris@pinola.co>
2014-05-05 23:50:21 -0400

    Adding logic to entity to detect if the current instance is a normal day and produce a list of unique colors (#3).

--- empy/src/main/java/co/pinola/empy/api/ColorInfo.java
+++ empy/src/main/java/co/pinola/empy/api/ColorInfo.java
@@ -1,5 +1,6 @@
 package co.pinola.empy.api;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,4 +14,44 @@ public class ColorInfo {
     public String event;
     public boolean manual; // not quite sure what this is for
 
+    public boolean IsNormalDay()
+    {
+        if (this.event != null) return false;
+
+        List<String> uniqueColors = this.GetUniqueColors();
+
+        if(uniqueColors.size() != 1) return false;
+
+        if(uniqueColors.get(0).equalsIgnoreCase("White")) return true;
+
+        return false;
+    }
+
+    public List<String> GetUniqueColors()
+    {
+        ArrayList<String> uniqueColors = new ArrayList<String>();
+
+        if (colors == null) return uniqueColors;
+
+        for (String color : colors)
+        {
+            if(!ListContainsString(uniqueColors, color))
+                uniqueColors.add(color);
+        }
+
+        return uniqueColors;
+    }
+
+    private boolean ListContainsString(List<String> list, String testString)
+    {
+        if (list == null) return false;
+
+        for (String string : list)
+        {
+            if (string.equalsIgnoreCase(testString))
+                return true;
+        }
+
+        return false;
+    }
 }
