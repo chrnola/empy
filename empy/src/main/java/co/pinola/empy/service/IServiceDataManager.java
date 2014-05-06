dec986fa8d6c0c1ccc6bf85e071ca0d445fd1db2
Chris Pinola <chris@pinola.co>
2014-05-05 23:48:08 -0400

    Updating interface to allow preferences object to be passed in (#3).

--- empy/src/main/java/co/pinola/empy/service/IServiceDataManager.java
+++ empy/src/main/java/co/pinola/empy/service/IServiceDataManager.java
@@ -1,10 +1,12 @@
 package co.pinola.empy.service;
 
+import android.content.SharedPreferences;
+
 /**
  * Created by @chrnola on 4/15/14.
  */
 public interface IServiceDataManager {
 
-    DashClockInfo GetUpdatedInfo(int reasonCode);
+    DashClockInfo GetUpdatedInfo(int reasonCode, SharedPreferences prefs);
 
 }
